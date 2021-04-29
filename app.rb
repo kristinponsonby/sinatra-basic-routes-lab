ac66030e483fdb1c90e3e30182f274c7cbd9b2ef
Kristin Ponsonby <kristinponsonby@gmail.com>
2021-04-28 21:16:01 -0400

    commit tree 78600d27433529e04b41349825058d7fa22437f1

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is KP."
+      end 
+
+      get '/hometown' do 
+        "My hometown is Houston, Texas"
+      end
+
+      get '/favorite-song' do 
+        "My favorite song is one more time by Daft Punk"
+      end
 end
+    
+ 
